747ac532326f26d21942af3bbc6e0c4477dbfa9f
Italo Canturil <italo.cantur@gmail.com>
2022-12-19 08:51:00 -0300

    feat: add Table component

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  
+  <header>
+    <h1>Pharma Inc</h1>
+    <Table :data="users"/>
+  </header>
 </template>
 
 <script setup>
+import Table from '../components/Table.vue';
+import useStore from '../stores'
 
+const store = useStore();
+const users = store.users;
 </script>
 
 <style>
